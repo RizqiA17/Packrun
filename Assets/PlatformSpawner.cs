570d0ddb8b13ff9a90fe2c39bd86717e2777f2c9
RizqiA17 <asyriansyah@gmail.com>
2025-03-07 10:03:38 +0700

    Ground Spawner

--- Assets/PlatformSpawner.cs
+++ Assets/PlatformSpawner.cs
@@ -3,20 +3,31 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject platform;
+    [SerializeField] private int levelLenght;
+    private float deltaTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        InitialSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        deltaTime = Time.deltaTime;
     }
 
-    private void OnTriggerEnter(Collider other)
+    void InitialSpawn()
     {
-        Instantiate(platform, new Vector3(80 - Time.deltaTime, 0, 0), Quaternion.Euler(Vector3.zero));
+        for (int i = 0; i < levelLenght; i++)
+        {
+            Instantiate(platform, new Vector3(i * 20, 0, 0), Quaternion.Euler(Vector3.zero));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Instantiate(platform, new Vector3(other.transform.position.x + (levelLenght * 20), 0, 0), Quaternion.Euler(Vector3.zero));
+        Destroy(other.gameObject);
     }
 }
